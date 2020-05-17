a247c088fcd096eb1cc1121a2c9f19950a808400
Dumaru <robinsonmu@unisabana.edu.co>
2020-05-17 13:55:59 -0500

    Gaze interaction scene

---
+++ Assets/2. Gaze Interaction/Scripts/Gaze.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour
+{
+    List<InfoBehaviour> infoBehaviours = new List<InfoBehaviour>();
+    Transform cameraTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        //  TODO: ADD DYNAMICALLY
+        infoBehaviours = FindObjectsOfType<InfoBehaviour>().ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out RaycastHit hit))
+        {
+            if (hit.collider.gameObject.CompareTag("HasInfo"))
+            {
+                OpenInfo(hit.collider.gameObject.GetComponent<InfoBehaviour>());
+            }
+        }
+        else
+        {
+            // No info behaviour was hitted
+            CloseAllInfos();
+        }
+    }
+
+    private void CloseAllInfos()
+    {
+        foreach (InfoBehaviour infoBehaviour in infoBehaviours)
+        {
+            infoBehaviour.CloseInfoSection();
+        }
+    }
+
+    void OpenInfo(InfoBehaviour desiredInfo)
+    {
+        foreach (InfoBehaviour infoBehaviour in infoBehaviours)
+        {
+            if (infoBehaviour == desiredInfo)
+            {
+                infoBehaviour.OpenInfoSection();
+            }
+            else
+            {
+                infoBehaviour.CloseInfoSection();
+            }
+        }
+    }
+}
---
+++ Assets/2. Gaze Interaction/Scripts/InfoBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoBehaviour : MonoBehaviour
+{
+    public const float SPEED = 6;
+    [SerializeField]
+    Transform sectionInfo;
+    Vector3 desiredScale = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sectionInfo.localScale = Vector3.Lerp(sectionInfo.localScale, desiredScale, Time.deltaTime * SPEED);
+    }
+
+    public void CloseInfoSection()
+    {
+        desiredScale = Vector3.zero;
+    }
+    public void OpenInfoSection()
+    {
+        desiredScale = Vector3.one;
+    }
+}
